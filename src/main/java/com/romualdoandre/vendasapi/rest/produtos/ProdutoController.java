504fb8742c882de53d5a3c60df685ac329075204
romualdo.andre <romualdo.andre@gmail.com>
2025-01-08 11:29:35 -0300

    configuração de cors

--- src/main/java/com/romualdoandre/vendasapi/rest/produtos/ProdutoController.java
+++ src/main/java/com/romualdoandre/vendasapi/rest/produtos/ProdutoController.java
@@ -1,6 +1,7 @@
 package com.romualdoandre.vendasapi.rest.produtos;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import com.romualdoandre.vendasapi.model.repository.ProdutoRepository;
 
 @RestController
 @RequestMapping("/api/produtos")
+@CrossOrigin("*")
 public class ProdutoController {
 	@Autowired
 	private ProdutoRepository repository;
