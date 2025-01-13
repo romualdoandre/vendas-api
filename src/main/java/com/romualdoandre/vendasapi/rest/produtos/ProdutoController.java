4a9b52a7bca918b982a937247354fb82e564b0e3
romualdo.andre <romualdo.andre@sec.ba.gov.br>
2025-01-13 15:58:13 -0300

    método atualizar

--- src/main/java/com/romualdoandre/vendasapi/rest/produtos/ProdutoController.java
+++ src/main/java/com/romualdoandre/vendasapi/rest/produtos/ProdutoController.java
@@ -1,8 +1,13 @@
 package com.romualdoandre.vendasapi.rest.produtos;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +29,16 @@ public class ProdutoController {
 		repository.save(entidadeProduto);
 		produto.setId(entidadeProduto.getId());
 		return ProdutoFormRequest.fromModel(entidadeProduto);
+	}
+	@PutMapping("{id}")
+	public ResponseEntity<Void> atualizar(@PathVariable Long id, @RequestBody ProdutoFormRequest produto) {
+		Optional<Produto> produtoExistente = repository.findById(id);
+		if(produtoExistente.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		Produto entidade = produto.toModel();
+		entidade.setId(id);
+		repository.save(entidade);
+		return ResponseEntity.ok().build();
 	}
 }
