d7e35f18625779b712a0f57acdb4125844acbf54
romualdoandre <romualdoandre@gmail.com>
2025-03-14 10:57:22 -0300

    configuração de user datils manager

--- src/main/java/com/romualdoandre/vendasapi/config/SecurityConfiguration.java
+++ src/main/java/com/romualdoandre/vendasapi/config/SecurityConfiguration.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,5 +24,15 @@ public class SecurityConfiguration {
             )
         .httpBasic(withDefaults());
         return http.build();
+    }
+	
+	@Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
